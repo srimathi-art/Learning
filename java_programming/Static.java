3d88c18146405e9b598b36e64062c4c35796c0f6
srimathi-art <srimathipalanisami392@gmail.com>
2026-07-23 15:59:19 +0530

    java

---
+++ java_programming/Static.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Static {
+    static int a=5;
+    public static void main (String[] args){
+        a = 7;
+        System.out.println(a);
+    }
+    
+}
---
+++ java_programming/args1_command.java
@@ -0,0 +1,6 @@
+import java.util.*;
+public class args1_command{
+    public static void main (String[] args){
+        System.out.println(args[0]);
+    }
+}
