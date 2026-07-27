2d087379402ace207b6b4224035dce34e8bd376f
srimathi-art <srimathipalanisami392@gmail.com>
2026-07-27 15:44:11 +0530

    java looping - odd numbers 2- 100

---
+++ java_programming/looping/odd.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class odd {
+    public static void main(String [] args){
+        for (int i =1; i<=100;i+=2){
+            System.out.println(i);
+        }
+    }
+    
+}
