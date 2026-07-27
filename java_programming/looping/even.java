a253814ae84291bf077561f7a3ed756a374e6392
srimathi-art <srimathipalanisami392@gmail.com>
2026-07-27 15:41:54 +0530

    java looping - even numbers 2-100

---
+++ java_programming/looping/even.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class even {
+    public static void main (String []args){
+        for (int i=2; i<=100;i+=2){
+            System.out.println(i);
+
+        }
+    }
+}
