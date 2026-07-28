bee619323c413de6538767488aa330817dd1148e
srimathi-art <srimathipalanisami392@gmail.com>
2026-07-28 14:34:02 +0530

    kadane's algorithm

---
+++ java_programming/kadane.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class kadane {
+
+    static int maxSubarraySum(int[] arr) {
+        int res = arr[0];
+  
+        // Outer loop for starting point of subarray
+        for (int i = 0; i < arr.length; i++) {
+            int currSum = 0;
+      
+            // Inner loop for ending point of subarray
+            for (int j = i; j < arr.length; j++) {
+                currSum = currSum + arr[j];
+              
+                // Update res if currSum is greater than res
+                res = Math.max(res, currSum);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, -8, 7, -1, 2, 3};
+        System.out.println(maxSubarraySum(arr));
+    }
+}
